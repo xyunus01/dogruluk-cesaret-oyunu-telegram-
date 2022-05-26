da7bcb1d9a3d508da63318025e4f9ab35c1814e2
bnyokluguna <105468449+bnyokluguna@users.noreply.github.com>
2022-05-26 17:14:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ K_G = Client(
 
 # Start Buttonu İcin Def Oluşturalım :)
 def button():
-	BUTTON=[[InlineKeyboardButton(text="Support ",url="t.me/gycyolcu")]]
-	BUTTON+=[[InlineKeyboardButton(text="Sahibim",url="t.me/evetbenim38")]]
+	BUTTON=[[InlineKeyboardButton(text="Support ",url="t.me/sohbetf")]]
+	BUTTON+=[[InlineKeyboardButton(text="Sahibim",url="t.me/sohbetf")]]
 	return InlineKeyboardMarkup(BUTTON)
 
 # Kullanıcı Start Komutunu Kullanınca Selam'layalım :)
@@ -37,7 +37,7 @@ def button():
 async def _(client, message):
 	user = message.from_user # Kullanıcın Kimliğini Alalım
 
-	await message.reply_text(text="**Merhaba {}!**\n\n__Ben Pyrogram Api İle Yazılmış Eğlence Botuyum :)__\n\n**Sahibim =>** [Bot'un sahibi ile iletişime geçin](https://t.me/Evetbenim38)\nDoğruluk mu? Cesaret mi? Oyun Komutu => /dc".format(
+	await message.reply_text(text="**Merhaba {}!**\n\n__Ben Pyrogram Api İle Yazılmış Eğlence Botuyum :)__\n\n**Sahibim =>** [Bot'un sahibi ile iletişime geçin](https://t.me/sohbetf)\nDoğruluk mu? Cesaret mi? Oyun Komutu => /dc".format(
 		user.mention, # Kullanıcı'nın Adı
 		),
 	disable_web_page_preview=True, # Etiketin Önizlemesi Olmaması İcin Kullanıyoruz
